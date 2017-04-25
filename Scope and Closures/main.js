3f262c5f2e612adc200493c93c7dde28958d5123
vishnuvenugopal007 <vishnu.venugopal@live.com>
2017-04-25 12:32:38 -0700

    MakeCounter function complete

--- Scope and Closures/main.js
+++ Scope and Closures/main.js
@@ -1,3 +1,5 @@
+//TypeWriter
+
 function typeWriter() {
   var begin = ''
   function type(string) {
@@ -10,3 +12,31 @@ function typeWriter() {
 const type = typeWriter()
 
 type()
+
+//MakeCounter
+
+function makeCounter(number) {
+
+  return{
+    current: function() {
+      return number
+    },
+    increment: function() {
+      //return ++number
+      number += 1
+      return number
+    },
+    decrement: function() {
+    //  return --number
+      number -= 1
+      return number
+    }
+  }
+}
+
+var counter = makeCounter(0)
+console.log(counter.current())
+console.log(counter.increment())
+console.log(counter.increment())
+console.log(counter.decrement())
+console.log(counter.current())
